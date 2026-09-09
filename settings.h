3e84ec54dba053cb1cfea0c5fd3353c5f2c3f2af
KorolevIvanMi <Ivankorol1512@yandex.ru>
2026-09-09 21:35:19 +0300

    add fields to struct Settings

--- settings.h
+++ settings.h
@@ -1,6 +1,19 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
+
+#include <QString>
+#include <QDir>
+#include <QTime>
+
 struct Settings{
+    QString file_mask;
+    qint16 input_files_mode;
+    QDir output_dir;
+    QDir input_dir;
+    qint16 repeat_files_names_mode;
+    bool repeat_coding_files;
+    QTime repeat_timer;
+    quint64 hex_code_mask;
 
 };
 
